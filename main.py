25bfcd52dfa0e06f100bf88644b9faefd3483b9d
Kirill Makarov <kirillozavrik537@gmail.com>
2021-02-05 11:44:04 +0300

    Добавил заливку кнопки,  изменил сообщение в рамке

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import TOP, BOTTOM, CENTER, SE
 window = tk.Tk()
 window.geometry("800x600")
+window.title('Крестики-Нолики')
 
 title = tk.Label(window, text = "GUI-Tic-Tac-Toe",
     font = ("InterFace XBold", 30)
@@ -12,6 +13,15 @@ button = tk.Button(
     width = 15,
     height = 2,
     font = "Arial 20 bold",
+    fg = '#000000',
+    bg = '#32CD32',
+    bd =  10,
+    highlightthickness=4, 
+    highlightcolor="#32CD32", 
+    highlightbackground="#32CD32", 
+    borderwidth=4
+    # borderwidth = '2',
+    # highlightcolor = 'black'
 )
 
 title.pack(side=TOP)
