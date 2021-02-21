c9b079e340df7ab8a9cfc5ef21997a0609fb5e51
Kirill Makarov <kirillozavrik537@gmail.com>
2021-02-21 20:31:17 +0300

    Добавили фреймы для виджетов экрана игры

--- game_frame.py
+++ game_frame.py
@@ -8,19 +8,20 @@ class GameFrame(Frame):
         # контейнере
         master.rowconfigure(0, weight=1)
         master.columnconfigure(0, weight=1)
-        label = Label(self, text="Label")
-        label2 = Label(self, text="Label2", bg="red")
+        top_bar = Frame(self, bg='green', height=50, width=100)
+        board = Frame(self, bg='yellow')
         # Параметр sticky дает возможность виджету занимать доступное место в
         # ячейке, расщиряясь за свои границы
-        label.grid(column=1, row=1, sticky="nsew")
-        label2.grid(column=0, row=0, sticky="nsew")
+        top_bar.grid(row=0, column=0, columnspan=5, sticky="nsew")
+        board.grid(row=1, column=0, columnspan=5, rowspan=3, sticky="nsew")
 
     def show(self):
         # Каждой колонке внутри сетки необходимо выдать weight
         # Это определяет, насколько широка ячейка
         # Например, столбец с weight=2 в два раза шире столбца с weight=1
-        self.columnconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1)
-        self.columnconfigure(1, weight=1)
+        self.rowconfigure(0, weight=0)
         self.rowconfigure(1, weight=1)
+
+        self.columnconfigure(0, weight=1)
+
         self.grid(sticky="nsew")
