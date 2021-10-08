e5f9f16bf1daba24603d0294be1ccbdade50c9c8
Kirill Makarov <kirillozavrik537@gmail.com>
2021-10-08 20:32:44 +0300

    Добавил выход из игры в меню

--- main_menu_frame.py
+++ main_menu_frame.py
@@ -1,4 +1,4 @@
-from tkinter import Frame, Label, Button, TOP, Y
+from tkinter import Frame, Label, Button, TOP, Y, RIGHT
 
 class MainMenuFrame(Frame):
     def __init__(self, master, on_start_game_btn_click):
@@ -21,9 +21,27 @@ class MainMenuFrame(Frame):
         borderwidth=4,
         command=on_start_game_btn_click
         )
+        self.quit_button = Button(
+        self,
+        text = 'Выйти из игры',
+        width = 15,
+        height = 2,
+        font = "Arial 20 bold",
+        fg = '#000000',
+        bg = '#ff0000',
+        bd =  10,
+        highlightthickness=4, 
+        highlightcolor="#ff0000", 
+        highlightbackground="#ff0000", 
+        borderwidth=4,
+        command=quit
+        )
 
     def show(self):
         self.title.pack(side=TOP)
         self.button.pack(side=TOP, expand=True)
         self.pack(expand=True, fill=Y)
+        self.quit_button.pack(side=TOP, expand=True)
+    def quit(self, master):
+        master.destroy()
     
